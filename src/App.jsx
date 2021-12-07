4f054cbf3edef8b7317dbd7b0c7b978a85eab488
Sam Billingham <sambillie.sb@gmail.com>
2021-12-07 10:08:53 +0000

    add denied screen for incorrect network connection

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,11 @@ const OPENSEA_LINK = '';
 const App = () => {
   // Render Methods
   const [currentAccount, setCurrentAccount] = useState("");
+  const [networkId, setNetworkId] = useState('0x4');
+  const [networkName, setNetworkName] = useState('*******');
   const [mintedAmount, setMintedAmount] = useState('/'.repeat(20));
   const [loading, setLoading] = useState(0);
-  const [network, setNetwork] = useState('*******');
+  
   const [message, setMessage] = useState('');
   let mintedNum = 0;
 
@@ -56,10 +58,19 @@ const App = () => {
       }
 
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      const chainId = await ethereum.request({ method: 'eth_chainId' });
 
+      setNetworkId(chainId);
+
+      // String, hex code of the chainId of the Rinkebey test network
+      const rinkebyChainId = "0x4"; 
+      if (chainId != rinkebyChainId) {
+        setNetworkId(chainId);
+      } else {
+        setNetworkName('**** RINKEBY    ')
+      }
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]); 
-      desc = '/access granted'
     } catch (error) {
       console.log(error)
     }
@@ -77,16 +88,16 @@ const App = () => {
         const signer = provider.getSigner();
         const cmdCodez = new ethers.Contract(CONTRACT_ADDRESS, cmdCodezNFT.abi, signer);
 
-
         let chainId = await ethereum.request({ method: 'eth_chainId' });
-        console.log("Connected to chain " + chainId);
+
+         setNetworkId(chainId);
 
         // String, hex code of the chainId of the Rinkebey test network
         const rinkebyChainId = "0x4"; 
-        if (chainId !== rinkebyChainId) {
-          alert("You are not connected to the Rinkeby Test Network!");
+        if (chainId != rinkebyChainId) {
+          setNetworkId(chainId);
         } else {
-          setNetwork('**** RINKEBY    ')
+          setNetworkName('**** RINKEBY    ')
         }
 
         cmdCodez.on("CmdCodezNFTMinted", (from, tokenId) => {
@@ -143,8 +154,8 @@ const App = () => {
   }
 
   const renderNotConnectedContainer = () => (
-    <div class="window window--block">
-      <div class="ascii ascii--intro">
+    <div className="window window--block">
+      <div className="ascii ascii--intro">
 
 ░█████╗░███╗░░░███╗██████╗░  ░█████╗░░█████╗░██████╗░███████╗███████╗
 ██╔══██╗████╗░████║██╔══██╗  ██╔══██╗██╔══██╗██╔══██╗██╔════╝╚════██║
@@ -161,9 +172,9 @@ const App = () => {
 
   const renderConnectedContainer = () => (
     <div>
-      <div class="interface">
-          <div class="window">
-            <div class="ascii ascii--access">
+      <div className="interface">
+          <div className="window">
+            <div className="ascii ascii--access">
 
     ░█████╗░░█████╗░░█████╗░███████╗░██████╗░██████╗
     ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -173,25 +184,38 @@ const App = () => {
     ╚═╝░░╚═╝░╚════╝░░╚════╝░╚══════╝╚═════╝░╚═════╝░
             </div>
           </div>
-          <div class="col">
-            <div class="window">
+          <div className="col">
+            <div className="window">
               <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
                 ~/terminal[x27] mint
               </button>
             </div>
-            <div class="window">
-              <div class="mint">{mintedAmount}</div>
+            <div className="window">
+              <div className="mint">{mintedAmount}</div>
             </div>
-            <div class="window window-p0">
-              <div class="load">
-                <span class="loaded">{'='.repeat(loading)}</span>
-                <span class="loading">{'='.repeat(20-loading)}</span>
+            <div className="window window-p0">
+              <div className="load">
+                <span className="loaded">{'='.repeat(loading)}</span>
+                <span className="loading">{'='.repeat(20-loading)}</span>
               </div>
             </div>
         </div>
       </div>
-      <div class="message">
+      <div className="message">
         <a href={message} target="_blank">{message}</a>
+      </div>
+    </div>
+  );
+
+    const renderDeniedContainer = () => (
+    <div className="window window--block">
+      <div className="ascii ascii--intro">
+██████╗░███████╗███╗░░██╗██╗███████╗██████╗░
+██╔══██╗██╔════╝████╗░██║██║██╔════╝██╔══██╗
+██║░░██║█████╗░░██╔██╗██║██║█████╗░░██║░░██║
+██║░░██║██╔══╝░░██║╚████║██║██╔══╝░░██║░░██║
+██████╔╝███████╗██║░╚███║██║███████╗██████╔╝
+╚═════╝░╚══════╝╚═╝░░╚══╝╚═╝╚══════╝╚═════╝░
       </div>
     </div>
   );
@@ -222,21 +246,24 @@ const App = () => {
   return (
     <div className="App">
       <audio autoPlay loop src={mp3}></audio>
-      <div class="bar bar--top">
-        <span> </span>
-        <span class="blink">{network}</span>
+      <div className="bar bar--top">
+        <span className="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24"><path fill="#222222" d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm1 16.057v-3.057h2.994c-.059 1.143-.212 2.24-.456 3.279-.823-.12-1.674-.188-2.538-.222zm1.957 2.162c-.499 1.33-1.159 2.497-1.957 3.456v-3.62c.666.028 1.319.081 1.957.164zm-1.957-7.219v-3.015c.868-.034 1.721-.103 2.548-.224.238 1.027.389 2.111.446 3.239h-2.994zm0-5.014v-3.661c.806.969 1.471 2.15 1.971 3.496-.642.084-1.3.137-1.971.165zm2.703-3.267c1.237.496 2.354 1.228 3.29 2.146-.642.234-1.311.442-2.019.607-.344-.992-.775-1.91-1.271-2.753zm-7.241 13.56c-.244-1.039-.398-2.136-.456-3.279h2.994v3.057c-.865.034-1.714.102-2.538.222zm2.538 1.776v3.62c-.798-.959-1.458-2.126-1.957-3.456.638-.083 1.291-.136 1.957-.164zm-2.994-7.055c.057-1.128.207-2.212.446-3.239.827.121 1.68.19 2.548.224v3.015h-2.994zm1.024-5.179c.5-1.346 1.165-2.527 1.97-3.496v3.661c-.671-.028-1.329-.081-1.97-.165zm-2.005-.35c-.708-.165-1.377-.373-2.018-.607.937-.918 2.053-1.65 3.29-2.146-.496.844-.927 1.762-1.272 2.753zm-.549 1.918c-.264 1.151-.434 2.36-.492 3.611h-3.933c.165-1.658.739-3.197 1.617-4.518.88.361 1.816.67 2.808.907zm.009 9.262c-.988.236-1.92.542-2.797.9-.89-1.328-1.471-2.879-1.637-4.551h3.934c.058 1.265.231 2.488.5 3.651zm.553 1.917c.342.976.768 1.881 1.257 2.712-1.223-.49-2.326-1.211-3.256-2.115.636-.229 1.299-.435 1.999-.597zm9.924 0c.7.163 1.362.367 1.999.597-.931.903-2.034 1.625-3.257 2.116.489-.832.915-1.737 1.258-2.713zm.553-1.917c.27-1.163.442-2.386.501-3.651h3.934c-.167 1.672-.748 3.223-1.638 4.551-.877-.358-1.81-.664-2.797-.9zm.501-5.651c-.058-1.251-.229-2.46-.492-3.611.992-.237 1.929-.546 2.809-.907.877 1.321 1.451 2.86 1.616 4.518h-3.933z"/></svg>
+            <span className="title">HEXAPOD distribution</span>
+        </span>
+        <span className="blink">{networkName}</span>
       </div>
       <div className="container">
-          {currentAccount === "" ? (
-            renderNotConnectedContainer()
-          ): (renderConnectedContainer() )}
+          { currentAccount === "" && renderNotConnectedContainer() }
+          { currentAccount !== "" && networkId === '0x4' && renderConnectedContainer()}
+          { currentAccount !== "" && networkId !== '0x4' && renderDeniedContainer()}
       </div>
-      <div class="bar">
-        <span class="">NODE >> 
-        <span class="blink"> N{ (Math.floor(Math.random() * 100) + 1)}</span>
+      <div className="bar">
+        <span className="">NODE >> 
+        <span className="blink"> N{ (Math.floor(Math.random() * 100) + 1)} >> {networkId}</span>
         </span>
-        <span class="">System status: 
-        <span class="blink">{currentAccount === "" ? " Ready" : " active" }</span>
+        <span className="">System status: 
+        <span className="blink">{currentAccount === "" ? " Ready" : " active" }</span>
         </span>
       </div>
     </div>
